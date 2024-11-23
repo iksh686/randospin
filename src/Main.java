28534f0c9be5e87a30ce6444b344b669867ee1f8
YourAverageLion <159409011+YourAverageLion@users.noreply.github.com>
2024-11-23 11:01:36 -0800

    working as of now

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Random rand = new Random();
         int userriddlenumb = 0;
+        int userriddlecount = rand.nextInt(0,4);
         System.out.println("Welcome to auraspin!");
         System.out.println("Spin Chances:");
         System.out.println("    Default - 50%");
@@ -20,27 +23,77 @@ public class Main {
         System.out.println("You need to solve 1 riddle per roll!");
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Do you want to roll(say yes or no)?");
-        String user_roll = input.nextLine();
-        if (user_roll.equals("yes")){
-//            if(userriddlenumb <= 0){
-//                System.out.println("You have to solve a riddle first");
-//            }
-            switch (userriddlenumb) {
-                case 0 -> {
-                    System.out.println("What gets wet while drying?");
-                }
-                case 1 -> {
-                    System.out.println("What begins with an 'e' and only contains one letter?");
-                }
-                case 2 -> {
-                    System.out.println("What belongs to you but is used by everyone you meet?");
-                }
-                case 3 -> {
 
+        while (true) {
+
+            System.out.println("Do you want to roll or do a riddle(riddle or roll)?");
+            String user_roll = input.nextLine();
+            if (user_roll.equals("roll")) {
+                if (userriddlenumb <= 0) {
+                    System.out.println("You have to solve a riddle first");
+
+                } else {
+                    System.out.println("i forgor");
                 }
             }
+            if (user_roll.contains("riddle") || userriddlenumb<=0 ){
+                switch (userriddlecount) {
+                    case 0 -> {
+                        System.out.println("What gets wet while drying?");
+                        String answer = input.nextLine();
+                        if (answer.contains("towel")) {
+                            userriddlenumb = userriddlenumb + 1;
+                            System.out.println("Correct! You have " + userriddlenumb + " roll(s)!");
+                            userriddlecount = rand.nextInt(0, 4);
+                        } else {
+                            userriddlenumb = userriddlenumb - 1;
+                            System.out.println("Your answer is WRONG! You just lost a point lil bro");
+                        }
+                    }
+                    case 1 -> {
+                        System.out.println("What begins with an 'e' and only contains one letter?");
+                        String answer = input.nextLine();
+                        if (answer.contains("envelope")) {
+                            userriddlenumb = userriddlenumb + 1;
+                            System.out.println("Correct! You have " + userriddlenumb + " roll(s)!");
+                            userriddlecount = rand.nextInt(0, 4);
+                        } else {
+                            userriddlenumb = userriddlenumb - 1;
+                            System.out.println("Your answer is WRONG! You just lost a point lil bro");
+                        }
+                    }
+                    case 2 -> {
+                        System.out.println("What belongs to you but is used by everyone you meet?");
+                        String answer = input.nextLine();
+                        if (answer.contains("name")) {
+                            userriddlenumb = userriddlenumb + 1;
+                            System.out.println("Correct! You have " + userriddlenumb + " roll(s)!");
+                            userriddlecount = rand.nextInt(0, 4);
+                        } else {
+                            userriddlenumb = userriddlenumb - 1;
+                            System.out.println("Your answer is WRONG! You just lost a point lil bro");
+                        }
+                    }
+                    case 3 -> {
+                        System.out.println("What has a head, a tail, is brown, and has no legs?");
+                        String answer = input.nextLine();
+                        if (answer.contains("penny")) {
+                            userriddlenumb = userriddlenumb + 1;
+                            System.out.println("Correct! You have " + userriddlenumb + " roll(s)!");
+                            userriddlecount = rand.nextInt(0, 4);
+                        } else {
+                            userriddlenumb = userriddlenumb - 1;
+                            System.out.println("Your answer is WRONG! You just lost a point lil bro");
+                        }
+                    }
+                }
+            }
+
+
+            System.out.println();
+
         }
-        System.out.println();
+
+
     }
 }
